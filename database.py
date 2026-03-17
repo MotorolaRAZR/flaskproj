7a16678d92f0c937ecd1c691e03749104866af27
grimax <grigmax06@gmail.com>
2026-03-17 22:03:54 +0300

    better formatting

--- database.py
+++ database.py
@@ -4,32 +4,40 @@ from werkzeug.security import generate_password_hash
 
 db_name = "zzz.db"
 
+
 def get_connection():
     return sqlite3.connect(db_name)
+
 
 def init_db():
     Con = get_connection()
     Cur = Con.cursor()
 
-    Cur.execute("""CREATE TABLE IF NOT EXISTS User(
+    Cur.execute(
+        """CREATE TABLE IF NOT EXISTS User(
         id INTEGER PRIMARY KEY,
         username TEXT NOT NULL UNIQUE,
         password TEXT NOT NULL
-    )""")
+    )"""
+    )
 
-    Cur.execute("""CREATE TABLE IF NOT EXISTS Roles(
+    Cur.execute(
+        """CREATE TABLE IF NOT EXISTS Roles(
         id INTEGER PRIMARY KEY,
         RoleName TEXT NOT NULL UNIQUE
-    )""")
+    )"""
+    )
 
-    Cur.execute("""CREATE TABLE IF NOT EXISTS UserRoles(
+    Cur.execute(
+        """CREATE TABLE IF NOT EXISTS UserRoles(
         userId INTEGER,
         roleId INTEGER,
         UNIQUE (userId, roleId),
         FOREIGN KEY (userId) REFERENCES User (id),
         FOREIGN KEY (roleId) REFERENCES Roles (id)
-    )""")
-    
+    )"""
+    )
+
     Cur.execute("INSERT OR IGNORE INTO Roles (RoleName) VALUES ('admin')")
     # rework soon Cur.execute("INSERT OR IGNORE INTO Roles (RoleName) VALUES ('member')")
     Con.commit()
@@ -41,27 +49,34 @@ def AddNewUser(Username, Password):
     Cur = Con.cursor()
     hashed = generate_password_hash(Password)
     try:
-        Cur.execute("""INSERT INTO User(username, password) VALUES(?, ?)""", (Username, hashed)) 
+        Cur.execute(
+            """INSERT INTO User(username, password) VALUES(?, ?)""", (Username, hashed)
+        )
         Con.commit()
-        return 0   
+        return 0
     except sqlite3.IntegrityError:
         return 1
     finally:
         Con.close()
 
-def FetchUserRoles(Username):                                          # defining a user's name in their role
+
+def FetchUserRoles(Username):  # defining a user's name in their role
     Con = get_connection()
     Cur = Con.cursor()
-    Cur.execute("""
+    Cur.execute(
+        """
         SELECT Roles.RoleName
         FROM User
         JOIN UserRoles ON User.id = UserRoles.userId
         JOIN Roles ON Roles.id = UserRoles.roleId
         WHERE User.username = ?
-    """, (Username,))
+    """,
+        (Username,),
+    )
     rows = Cur.fetchall()
     Con.close()
     return [row[0] for row in rows]
+
 
 def FetchUsers(Username):
     Con = get_connection()
@@ -75,6 +90,7 @@ def FetchUsers(Username):
     Con.close()
     return None
 
+
 def AssignRole(UserName, RoleName):
     Con = get_connection()
     Cur = Con.cursor()
@@ -82,7 +98,7 @@ def AssignRole(UserName, RoleName):
     #  userid
     Cur.execute("SELECT id FROM User WHERE username = ?", (UserName,))
     UserResult = Cur.fetchone()
-    
+
     # roleid
     Cur.execute("SELECT id FROM Roles WHERE RoleName = ?", (RoleName,))
     RoleResult = Cur.fetchone()
@@ -92,12 +108,16 @@ def AssignRole(UserName, RoleName):
         RoleId = RoleResult[0]
         try:
             # duplicate check
-            Cur.execute("INSERT OR IGNORE INTO UserRoles(userId, roleId) VALUES(?,?)", (UserId, RoleId))
+            Cur.execute(
+                "INSERT OR IGNORE INTO UserRoles(userId, roleId) VALUES(?,?)",
+                (UserId, RoleId),
+            )
             Con.commit()
         except sqlite3.Error as Error:
             print(f"Error: {Error}")
-    
+
     Con.close()
+
 
 AddNewUser("admin", "admin")
 AssignRole("admin", "admin")
--- flask_app.py
+++ flask_app.py
@@ -4,64 +4,78 @@ import os
 import secrets
 from werkzeug.security import check_password_hash
 
-app = flask.Flask('__main__')
+app = flask.Flask("__main__")
 app.secret_key = secrets.token_hex(16)
 
-@app.route("/", methods=['POST', "GET"])
+
+@app.route("/", methods=["POST", "GET"])
 def main():
     returnvalue = ""
-    logged = flask.session.get('username') 
+    logged = flask.session.get("username")
     roles = database.FetchUserRoles(logged)
 
     if flask.request.method == "POST":
-        if 'username' in flask.request.form:
-            inputfromUser = flask.request.form['username']
+        if "username" in flask.request.form:
+            inputfromUser = flask.request.form["username"]
             returnvalue = database.FetchUsers(inputfromUser)
         else:
             returnvalue = ""
 
-        if "makeUser" in flask.request.form and "makepassword" in flask.request.form and "role" in flask.request.form:
-            name2Create = flask.request.form['makeUser']
-            pass2Create = flask.request.form['makepassword']
-            role = flask.request.form['role']
+        if (
+            "makeUser" in flask.request.form
+            and "makepassword" in flask.request.form
+            and "role" in flask.request.form
+        ):
+            name2Create = flask.request.form["makeUser"]
+            pass2Create = flask.request.form["makepassword"]
+            role = flask.request.form["role"]
             database.AddNewUser(name2Create, pass2Create)
             database.AssignRole(name2Create, role)
 
-        return flask.render_template("main.html", loggedIn=logged, returnedName=returnvalue, roles=roles)
+        return flask.render_template(
+            "main.html", loggedIn=logged, returnedName=returnvalue, roles=roles
+        )
 
     # another return if other one fails when we create user
-    return flask.render_template("main.html", loggedIn=logged, roles=roles, returnedName=returnvalue)
+    return flask.render_template(
+        "main.html", loggedIn=logged, roles=roles, returnedName=returnvalue
+    )
+
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
     sesh = flask.session
     if flask.request.method == "POST":
-        if 'username' in flask.request.form and 'password' in flask.request.form:
-            username = flask.request.form['username']
-            password = flask.request.form['password']
+        if "username" in flask.request.form and "password" in flask.request.form:
+            username = flask.request.form["username"]
+            password = flask.request.form["password"]
             answer = database.FetchUsers(username)
             print(answer)
             if answer != None:
                 if answer[1] == username and check_password_hash(answer[2], password):
-                    flask.session['username'] = username                                # put username into username
-                    flask.session['roles'] = database.FetchUserRoles(username)          # put roles into the session
+                    flask.session["username"] = username  # put username into username
+                    flask.session["roles"] = database.FetchUserRoles(
+                        username
+                    )  # put roles into the session
                     return flask.redirect(flask.url_for("main"))
         else:
             pass
-        if 'logout' in flask.request.form:
+        if "logout" in flask.request.form:
             print("aaaaa")
             return flask.redirect("/logout")
     print(flask.session)
     return flask.render_template("login.html", session=sesh)
 
-@app.route("/logout", methods=['POST', 'GET'])
+
+@app.route("/logout", methods=["POST", "GET"])
 def logout():
     if flask.request.method == "GET":
-        flask.session.pop('username', None)
-        flask.session.pop('roles', None)
+        flask.session.pop("username", None)
+        flask.session.pop("roles", None)
         flask.session.clear()
     return flask.redirect(flask.url_for("main"))
 
-# TODO MAYBE ADD WEBSOCKET CHATROOM
+
+# TODo MAYBE ADD WEBSOCKET CHATROOM
 
 app.run(debug=True)
