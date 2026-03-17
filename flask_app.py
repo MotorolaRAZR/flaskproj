cd92d5884e076b195ce42173dcd4a9be1c9c3e17
grimax <grigmax06@gmail.com>
2026-03-17 21:14:44 +0300

    fix

--- flask_app.py
+++ flask_app.py
@@ -7,16 +7,12 @@ from werkzeug.security import check_password_hash
 app = flask.Flask('__main__')
 app.secret_key = secrets.token_hex(16)
 
-database.init_db()
-database.AddNewUser("admin", "admin")
-database.AssignRole("admin", "admin")       # created an admin role XD
-
 @app.route("/", methods=['POST', "GET"])
 def main():
     returnvalue = ""
-    logged = flask.session.get('username')
+    logged = flask.session.get('username') 
     roles = database.FetchUserRoles(logged)
-    print(roles)
+
     if flask.request.method == "POST":
         if 'username' in flask.request.form:
             inputfromUser = flask.request.form['username']
@@ -33,30 +29,37 @@ def main():
 
         return flask.render_template("main.html", loggedIn=logged, returnedName=returnvalue, roles=roles)
 
-    # GET method
+    # another return if other one fails when we create user
     return flask.render_template("main.html", loggedIn=logged, roles=roles, returnedName=returnvalue)
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
+    sesh = flask.session
     if flask.request.method == "POST":
-        username = flask.request.form['username']
-        password = flask.request.form['password']
-
-        answer = database.FetchUsers(username)
-        print(answer)
-        if answer != None:
-            if answer[1] == username and check_password_hash(answer[2], password):
-                flask.session['username'] = username                                # put username into username
-                flask.session['roles'] = database.FetchUserRoles(username)          # put roles into the session
-                return flask.redirect(flask.url_for("main"))
+        if 'username' in flask.request.form and 'password' in flask.request.form:
+            username = flask.request.form['username']
+            password = flask.request.form['password']
+            answer = database.FetchUsers(username)
+            print(answer)
+            if answer != None:
+                if answer[1] == username and check_password_hash(answer[2], password):
+                    flask.session['username'] = username                                # put username into username
+                    flask.session['roles'] = database.FetchUserRoles(username)          # put roles into the session
+                    return flask.redirect(flask.url_for("main"))
+        else:
+            pass
+        if 'logout' in flask.request.form:
+            print("aaaaa")
+            return flask.redirect("/logout")
     print(flask.session)
-    return flask.render_template("login.html", loggedIn=flask.session.get('username'))
+    return flask.render_template("login.html", session=sesh)
 
 @app.route("/logout", methods=['POST', 'GET'])
 def logout():
-    if flask.request.method == "POST":
+    if flask.request.method == "GET":
         flask.session.pop('username', None)
         flask.session.pop('roles', None)
-    return flask.redirect(flask.url_for("login"))
+        flask.session.clear()
+    return flask.redirect(flask.url_for("main"))
 
 app.run(debug=True)
